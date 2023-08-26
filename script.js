8aeca415ffcfa0309d682daebad64fe0b68ffa03
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-08-26 02:35:17 -0500

    Theme toggle bug fix

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
-var root = document.querySelector(':root');
-var desktopTheme = document.getElementById('theme-switch');
-var mobileTheme = document.getElementById('mobile-theme-switch');
-
-function setDefaultTheme() {
-  //default theme
-  localStorage.setItem("theme", "day-theme");
-};
-
-setDefaultTheme();
+const root = document.querySelector(':root');
+const desktopTheme = document.querySelector('.theme-switch');
+const mobileTheme = document.querySelector('.mobile-theme-switch');
+const desktopNavbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.mobile-navbar');
+const desktopNavStyle = getComputedStyle(desktopNavbar);
+const mobileNavStyle = getComputedStyle(mobileNavbar);
 
 window.onload = loadTheme();
 
 function loadTheme() {
-  var localTheme = localStorage.getItem("theme");
+  const localTheme = localStorage.getItem("theme");
 
   if(localTheme !== null && localTheme === "night-theme"){
     root.classList.remove("day-theme");
     root.classList.add(localTheme);
+
+    desktopTheme.checked = true;
+    mobileTheme.checked = true;
   } 
   // day theme is default, no need to load it.
 };
@@ -30,66 +30,99 @@ function replaceTheme(a, b) {
 };
 
 function changeThemes() {
+  //check if it's on desktop view
 
-  const currentTheme = root.classList.contains("day-theme");
-
-  if(currentTheme !== null && currentTheme === true){
-    replaceTheme("day-theme","night-theme");
+  if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
+    if(desktopTheme.checked){
+      replaceTheme("night-theme","day-theme");
+      mobileTheme.checked = false;
+    }
+    else{
+      replaceTheme("day-theme","night-theme");
+      mobileTheme.checked = true;
+    }
   }
-  else{
-    replaceTheme("night-theme","day-theme");
+  
+  if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
+    if(mobileTheme.checked){
+      replaceTheme("night-theme","day-theme");
+      desktopTheme.checked = false;
+    }
+    else{
+      replaceTheme("day-theme","night-theme");
+      desktopTheme.checked = true;
+    }
   }
 
 };
 
 // navbar and theme toggle is hidden on scroll
-var navbars = document.querySelectorAll('navbar');
-var themeToggle = document.querySelector('theme-toggle-button-container');
+const themeToggle = document.querySelector('.theme-toggle-button-container');
 var hideScroll = window.scrollY;
 
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
   
-  navbars.forEach((nav) => {
-    if(nav.style.display != "none"){
-      if (hideScroll > window.scrollY) {
-        nav.style.top = "0";
-        //we will also update the Y position of the theme toggle here too
-        themeToggle.style.bottom = "1.5rem";
-      } 
-      else {
-        nav.style.top = "-10rem";
-        themeToggle.style.bottom = "-6rem";
-      }
+  if(desktopNavbar.style.display !== "none"){
+    if (hideScroll > window.scrollY) {
+      desktopNavbar.style.top = "0";
+      //we will also update the Y position of the theme toggle here too
+      themeToggle.style.bottom = "1.5rem";
+    } 
+    else {
+      desktopNavbar.style.top = "-10rem";
+      themeToggle.style.bottom = "-6rem";
     }
-  })
+  }
 
   hideScroll = currentScrollPos;
 };
 
-const navItemBtns = document.querySelectorAll('menu-item-container'); 
-const navPages = document.querySelectorAll("menu-item");
+const desktopItemBtns = document.querySelectorAll('.menu-item-container'); 
+const desktopPages = document.querySelectorAll('.menu-item');
+const mobileItemBtns = document.querySelectorAll('.mobile-menu-item-container'); 
+const mobilePages = document.querySelectorAll('.mobile-menu-item');
 let currentURL = window.location.href;
 
 // reading url to determine which buttons to highlight
 
 function btnHighlighs (){
-  // Removes the "menu-item-container-style" class from all buttons in the navItemBtns array using the forEach method.
-  navItemBtns.forEach((btn) => {
-    btn.classList.remove('menu-item-container-style');
-  });
+  // Removes the "(mobile-)menu-item-container-style" class from all buttons in the navItemBtns array
+  
+  if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
+    desktopItemBtns.forEach((btn) => {
+      btn.classList.remove('menu-item-container-style');
+    });
+    desktopPages.forEach((page) => {
+      let navPage = page.dataset.id;
+      if(currentURL.includes("index") && navPage == "home"){
+        let parent = page.parentElement;
+        parent.classList.add('menu-item-container-style');
+      }
+      else if(currentURL.includes(navPage)){
+        let parent = page.parentElement;
+        parent.classList.add('menu-item-container-style');
+      }
+    });
+  }
 
-  navPages.forEach((page) => {
-    let navPage = page.dataset.id;
-    if(currentURL.includes("index") && navPage == "home"){
-      let parent = page.parentElement;
-      parent.classList.add('menu-item-container-style');
-    }
-    else if(currentURL.includes(navPage)){
-      let parent = page.parentElement;
-      parent.classList.add('menu-item-container-style');
-    }
-  });
+  if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
+    mobileItemBtns.forEach((btn) => {
+      btn.classList.remove('mobile-menu-item-container-style');
+    });
+    mobilePages.forEach((page) => {
+      let navPage = page.dataset.id;
+      if(currentURL.includes("index") && navPage == "home"){
+        let parent = page.parentElement;
+        parent.classList.add('mobile-menu-item-container-style');
+      }
+      else if(currentURL.includes(navPage)){
+        let parent = page.parentElement;
+        parent.classList.add('mobile-menu-item-container-style');
+      }
+    });
+  }
+
 }
 
 btnHighlighs();
