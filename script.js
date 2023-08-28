aa6b6c2e30bb10f2bef3ee0ba7ef9f3101426e0f
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-08-28 04:33:45 -0500

    Redesigned the mobile nav bar and logo

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 
 const root = document.querySelector(':root');
-const desktopTheme = document.querySelector('.theme-switch');
-const mobileTheme = document.querySelector('.mobile-theme-switch');
+var desktopTheme = document.querySelector('.theme-switch');
+var mobileTheme = document.querySelector('.mobile-theme-switch');
 const desktopNavbar = document.querySelector('.navbar');
-const mobileNavbar = document.querySelector('.mobile-navbar');
 const desktopNavStyle = getComputedStyle(desktopNavbar);
+const mobileNavbar = document.querySelector('.mobile-navbar');
 const mobileNavStyle = getComputedStyle(mobileNavbar);
 
 window.onload = loadTheme();
 
 function loadTheme() {
   const localTheme = localStorage.getItem("theme");
-
   if(localTheme !== null && localTheme === "night-theme"){
     root.classList.remove("day-theme");
     root.classList.add(localTheme);
-
     desktopTheme.checked = true;
     mobileTheme.checked = true;
   } 
@@ -23,6 +21,7 @@ function loadTheme() {
 };
 
 //helper function
+
 function replaceTheme(a, b) {
   root.classList.remove(a);
   root.classList.add(b);
@@ -30,8 +29,6 @@ function replaceTheme(a, b) {
 };
 
 function changeThemes() {
-  //check if it's on desktop view
-
   if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
     if(desktopTheme.checked){
       replaceTheme("night-theme","day-theme");
@@ -42,7 +39,6 @@ function changeThemes() {
       mobileTheme.checked = true;
     }
   }
-  
   if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
     if(mobileTheme.checked){
       replaceTheme("night-theme","day-theme");
@@ -53,19 +49,19 @@ function changeThemes() {
       desktopTheme.checked = true;
     }
   }
-
 };
 
 // navbar and theme toggle is hidden on scroll
+
 const themeToggle = document.querySelector('.theme-toggle-button-container');
 var hideScroll = window.scrollY;
 
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
-  
   if(desktopNavbar.style.display !== "none"){
     if (hideScroll > window.scrollY) {
       desktopNavbar.style.top = "0";
+
       //we will also update the Y position of the theme toggle here too
       themeToggle.style.bottom = "1.5rem";
     } 
@@ -74,9 +70,10 @@ window.onscroll = function() {
       themeToggle.style.bottom = "-6rem";
     }
   }
-
   hideScroll = currentScrollPos;
 };
+
+// reading url to determine which buttons to highlight
 
 const desktopItemBtns = document.querySelectorAll('.menu-item-container'); 
 const desktopPages = document.querySelectorAll('.menu-item');
@@ -84,11 +81,7 @@ const mobileItemBtns = document.querySelectorAll('.mobile-menu-item-container');
 const mobilePages = document.querySelectorAll('.mobile-menu-item');
 let currentURL = window.location.href;
 
-// reading url to determine which buttons to highlight
-
 function btnHighlighs (){
-  // Removes the "(mobile-)menu-item-container-style" class from all buttons in the navItemBtns array
-  
   if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
     desktopItemBtns.forEach((btn) => {
       btn.classList.remove('menu-item-container-style');
@@ -105,7 +98,6 @@ function btnHighlighs (){
       }
     });
   }
-
   if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
     mobileItemBtns.forEach((btn) => {
       btn.classList.remove('mobile-menu-item-container-style');
@@ -122,7 +114,37 @@ function btnHighlighs (){
       }
     });
   }
-
-}
+};
 
 btnHighlighs();
+
+// makes the names of the pages visible or unvisible when toggled
+
+var menuSwitch = document.querySelector('.menu-switch');
+var mobileMenuSwitch = document.querySelector('.mobile-menu-switch');
+
+menuSwitch.addEventListener('click', () => {
+  if(menuSwitch.checked){
+    mobilePages.forEach((item) => {
+      item.classList.remove('mobile-menu-item');
+      item.classList.add('mobile-menu-item-after');
+    });
+  }
+  else{
+    mobilePages.forEach((item) => {
+      item.classList.add('mobile-menu-item');
+      item.classList.remove('mobile-menu-item-after');
+    });
+  }
+});
+
+// makes the names of the pages visible or unvisible when toggled
+
+menuSwitch.addEventListener('click', () => {
+  if(menuSwitch.checked){
+    mobileMenuSwitch.style.setProperty("justify-content","flex-end");
+  }
+  else{
+    mobileMenuSwitch.style.setProperty("justify-content","center");
+  }
+});
