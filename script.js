5c9fb4da3ea43580ebdc010e59f41a88369c80c2
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2023-09-01 02:46:33 -0500

    Changed sidebar to hamburger menu

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 
 const root = document.querySelector(':root');
-var desktopTheme = document.querySelector('.theme-switch');
-var mobileTheme = document.querySelector('.mobile-theme-switch');
+var desktopTheme = document.querySelector('.theme-toggle');
 const desktopNavbar = document.querySelector('.navbar');
-const desktopNavStyle = getComputedStyle(desktopNavbar);
-const mobileNavbar = document.querySelector('.mobile-navbar');
-const mobileNavStyle = getComputedStyle(mobileNavbar);
 
 window.onload = loadTheme();
 
@@ -15,7 +11,6 @@ function loadTheme() {
     root.classList.remove("day-theme");
     root.classList.add(localTheme);
     desktopTheme.checked = true;
-    mobileTheme.checked = true;
   } 
   // day theme is default, no need to load it.
 };
@@ -29,35 +24,43 @@ function replaceTheme(a, b) {
 };
 
 function changeThemes() {
-  if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
-    if(desktopTheme.checked){
-      replaceTheme("night-theme","day-theme");
-      mobileTheme.checked = false;
-    }
-    else{
-      replaceTheme("day-theme","night-theme");
-      mobileTheme.checked = true;
-    }
+  if(desktopTheme.checked){
+    replaceTheme("night-theme","day-theme");
   }
-  if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
-    if(mobileTheme.checked){
-      replaceTheme("night-theme","day-theme");
-      desktopTheme.checked = false;
-    }
-    else{
-      replaceTheme("day-theme","night-theme");
-      desktopTheme.checked = true;
-    }
+  else{
+    replaceTheme("day-theme","night-theme");
   }
 };
 
-// navbar and theme toggle is hidden on scroll
+var openMenuSVG = document.querySelector('.open-menu-svg');
+var closeMenuSVG = document.querySelector('.close-menu-svg');
+var menuToggle = document.getElementById('mobile-menu-toggle');
+function changeMenuToggleStyle() {
+  if(menuToggle.checked){
+    openMenuSVG.style.display = "none";
+    closeMenuSVG.style.display = "block";
+  }
+  else{
+    openMenuSVG.style.display = "block";
+    closeMenuSVG.style.display = "none";
+  }
+}
+menuToggle.addEventListener('click', changeMenuToggleStyle);
 
+const nightIcon = document.getElementById('night-theme-icon');
+const dayIcon = document.getElementById('day-theme-icon');
+
+nightIcon.addEventListener('click', changeThemes);
+dayIcon.addEventListener('click', changeThemes);
+
+// navbar and theme toggle is hidden on scroll
+/*
 var hideScroll = window.scrollY;
 
 window.onscroll = function() {
+  const pageWidth  = document.body.clientWidth;
   var currentScrollPos = window.scrollY;
-  if(desktopNavbar.style.display !== "none"){
+  if(desktopNavbar.style.display !== "none" && pageWidth > 870){
     if (hideScroll > window.scrollY) {
       desktopNavbar.style.top = "0";
     } 
@@ -67,17 +70,15 @@ window.onscroll = function() {
   }
   hideScroll = currentScrollPos;
 };
-
+*/
 // reading url to determine which buttons to highlight
 
 const desktopItemBtns = document.querySelectorAll('.menu-item-container'); 
 const desktopPages = document.querySelectorAll('.menu-item');
-const mobileItemBtns = document.querySelectorAll('.mobile-menu-item-container'); 
-const mobilePages = document.querySelectorAll('.mobile-menu-item');
 let currentURL = window.location.href;
 
 function btnHighlighs (){
-  if(desktopNavStyle.display !== null && desktopNavStyle.display === "flex"){
+  if(desktopNavbar.style.display !== null){
     desktopItemBtns.forEach((btn) => {
       btn.classList.remove('menu-item-container-style');
     });
@@ -93,51 +94,6 @@ function btnHighlighs (){
       }
     });
   }
-  if(mobileNavStyle.display !== null && mobileNavStyle.display === "flex"){
-    mobileItemBtns.forEach((btn) => {
-      btn.classList.remove('mobile-menu-item-container-style');
-    });
-    mobilePages.forEach((page) => {
-      let navPage = page.dataset.id;
-      if(currentURL.includes("index") && navPage == "home"){
-        let parent = page.parentElement;
-        parent.classList.add('mobile-menu-item-container-style');
-      }
-      else if(currentURL.includes(navPage)){
-        let parent = page.parentElement;
-        parent.classList.add('mobile-menu-item-container-style');
-      }
-    });
-  }
 };
 
 btnHighlighs();
-
-// makes the mobile navbar open and close
-
-var menuSwitch = document.querySelector('.menu-switch');
-var mobileMenuSwitch = document.querySelector('.mobile-menu-switch');
-
-menuSwitch.addEventListener('click', () => {
-  if(menuSwitch.checked){
-    mobilePages.forEach((item) => {
-      item.classList.remove('mobile-menu-item');
-      item.classList.add('mobile-menu-item-after');
-    });
-  }
-  else{
-    mobilePages.forEach((item) => {
-      item.classList.add('mobile-menu-item');
-      item.classList.remove('mobile-menu-item-after');
-    });
-  }
-});
-
-menuSwitch.addEventListener('click', () => {
-  if(menuSwitch.checked){
-    mobileMenuSwitch.style.setProperty("justify-content","flex-end");
-  }
-  else{
-    mobileMenuSwitch.style.setProperty("justify-content","center");
-  }
-});
